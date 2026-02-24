4357fe71cdae6a95c2071a9522087184c3e8db10
b23kk <168123285+b23kk@users.noreply.github.com>
2026-02-24 23:23:15 +0100

    Update Untitled-2.ps1

--- Untitled-2.ps1
+++ Untitled-2.ps1
@@ -4,101 +4,113 @@ $DisableServices = @(
     "AMD Crash Defender Service"
     "AMD External Events Utility"
     "DiagTrack"
-    "dmwappushservice" 
-    "lfsvc" 
+    "dmwappushservice"
+    "lfsvc"
     "MapsBroker"
-    "NetTcpPortSharing" 
-    "RemoteAccess" 
-    "RemoteRegistry" 
-    "SharedAccess" 
-    "TrkWks" 
-    "WbioSrvc" 
-    "ndu" 
-    "WerSvc" 
-    "Spooler" 
-    "Fax" 
-    "fhsvc" 
-    "gupdate" 
-    "gupdatem" 
-    "stisvc" 
-    "AJRouter" 
-    "MSDTC" 
-    "WpcMonSvc" 
-    "PhoneSvc" 
-    "PcaSvc" 
-    "WPDBusEnum" 
-    "seclogon" 
-    "lmhosts" 
-    "wisvc" 
-    "FontCache" 
-    "RetailDemo" 
-    "ALG" 
-    "SCardSvr" 
-    "SCPolicySvc" 
-    "ScDeviceEnum" 
-    "MessagingService_34048" 
-    "EntAppSvc" 
-    "Browser" 
-    "BDESVC" 
-    "iphlpsvc" 
-    "edgeupdate" 
-    "MicrosoftEdgeElevationService" 
-    "edgeupdatem" 
-    "SEMgrSvc" 
-    "PerfHost" 
-    "BcastDVRUserService_48486de" 
-    "WpnService" 
-    "DoSvc" 
-    "SNMPTrap" 
-    "SECOMNService" 
-    "autotimesvc" 
-    "tzautoupdate" 
-    "BcastDVRUserService_34048" 
-    "PenService_34048" 
-    "tapisrv" 
-    "HvHost" 
-    "vmickvpexchange" 
-    "vmicguestinterface" 
-    "vmicshutdown" 
-    "vmicheartbeat" 
-    "vmicvmsession" 
-    "vmicrdv" 
+    "NetTcpPortSharing"
+    "RemoteAccess"
+    "RemoteRegistry"
+    "SharedAccess"
+    "TrkWks"
+    "WbioSrvc"
+    "ndu"
+    "WerSvc"
+    "Spooler"
+    "Fax"
+    "fhsvc"
+    "gupdate"
+    "gupdatem"
+    "stisvc"
+    "AJRouter"
+    "MSDTC"
+    "WpcMonSvc"
+    "PhoneSvc"
+    "PcaSvc"
+    "WPDBusEnum"
+    "seclogon"
+    "lmhosts"
+    "wisvc"
+    "FontCache"
+    "RetailDemo"
+    "ALG"
+    "SCardSvr"
+    "SCPolicySvc"
+    "ScDeviceEnum"
+    "MessagingService_34048"
+    "EntAppSvc"
+    "Browser"
+    "BDESVC"
+    "iphlpsvc"
+    "edgeupdate"
+    "MicrosoftEdgeElevationService"
+    "edgeupdatem"
+    "SEMgrSvc"
+    "PerfHost"
+    "BcastDVRUserService_48486de"
+    "WpnService"
+    "DoSvc"
+    "SNMPTrap"
+    "SECOMNService"
+    "autotimesvc"
+    "tzautoupdate"
+    "BcastDVRUserService_34048"
+    "PenService_34048"
+    "tapisrv"
+    "HvHost"
+    "vmickvpexchange"
+    "vmicguestinterface"
+    "vmicshutdown"
+    "vmicheartbeat"
+    "vmicvmsession"
+    "vmicrdv"
     "vmictimesync"
 )
 
 Write-Host "====================" -ForegroundColor DarkGray
 Write-Host "[1]" -ForegroundColor Cyan -NoNewline
-Write-Host " Disable Services" 
+Write-Host " Disable Services"
 Write-Host "[2]" -ForegroundColor Cyan -NoNewline
 Write-Host " Show Services"
-Write-Host '[3]' -ForegroundColor Cyan -NoNewline
+Write-Host "[3]" -ForegroundColor Cyan -NoNewline
 Write-Host " Exit"
 Write-Host "====================" -ForegroundColor DarkGray
 
-$Chose = Read-Host "Choose an Option"
+$Choose = Read-Host "Choose an Option"
 
 switch ($Choose) {
-    "1" { 
-        Write-Host "Disabling: " -NoNewline
-        Write-Host "$DisableServices" -ForegroundColor blue
+
+    "1" {
         foreach ($Service in $DisableServices) {
-            $service = Get-Service -Name $Service -ErrorAction SilentlyContinue
-        }
-        "2" { 
-            Write-Host "Showing Services"
-            foreach ($Service in $DisableServices) {
-                $service = Get-Service -Name $Service -ErrorAction SilentlyContinue
-                if ($service) {
-                    Write-Host "$($service.Name) - $($service.Status)" -ForegroundColor blue
-                } else {
-                    Write-Host "$Service - Not Found" -ForegroundColor red
+            $svc = Get-Service -Name $Service -ErrorAction SilentlyContinue
+            if ($svc) {
+                if ($svc.Status -ne "Stopped") {
+                    Stop-Service -Name $svc.Name -Force
                 }
+                Set-Service -Name $svc.Name -StartupType Disabled
+                Write-Host "$($svc.Name) - Disabled" -ForegroundColor Blue
             }
         }
-        "3" { 
-            Write-Host "Exiting..."
-            Start-Sleep -Seconds 2
-            exit
+    }
+
+    "2" {
+        Write-Host "Showing Services"
+        foreach ($Service in $DisableServices) {
+            $svc = Get-Service -Name $Service -ErrorAction SilentlyContinue
+            if ($svc) {
+                Write-Host "$($svc.Name) - $($svc.Status)" -ForegroundColor Blue
+            } else {
+                Write-Host "$Service - Not Found" -ForegroundColor Red
+            }
         }
+    }
+
+    "3" {
+        Write-Host "Exiting..."
+        Start-Sleep 2
+        exit
+    }
+
+    Default {
+        Write-Host "Invalid Option" -ForegroundColor Red
     }
 }
